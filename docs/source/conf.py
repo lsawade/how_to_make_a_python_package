bdc7475e5820f59378f780eb3cb5390305be496d
Lucas Sawade <lsawade@princeton.edu>
2020-04-20 01:12:49 -0400

    check

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 # html_theme = 'sphinx_rtd_theme'
-html_theme = 'crate'
+html_theme = 'crate-docs-theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
