0211c07c8fbd762d79c44d1d6affeb5bcda50d8d
PeterMakus <peter.makus@gmx.de>
2021-03-25 10:58:33 +0100

    update gitignore and installation documentation

--- setup.py
+++ setup.py
@@ -6,5 +6,25 @@ environment specified in that file.
 
 """
 from setuptools import setup
+from setuptools.command.test import test as testcommand
 
-setup()
+# This installs the pytest command. Meaning that you can simply type pytest
+# anywhere and "pytest" will look for all available tests in the current
+# directory and subdirectories recursively (not yet supported in the setup.cfg)
+
+
+class PyTest(testcommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.tests")]
+
+    def initialize_options(self):
+        testcommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        import pytest
+        import sys
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(cmdclass={'tests': PyTest})
